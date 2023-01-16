71eabc2426504cff222debe230d0108075f9bc9b
Daniel Muñoz Martín <info@danimm.com>
2023-01-16 09:53:11 +0100

    refactor

--- index.js
+++ index.js
@@ -1,56 +1,38 @@
-const h1 = document.querySelector('h1');
-const form = document.querySelector('#form');
-const numero1 = document.querySelector('#calculo1');
-const numero2 = document.querySelector('#calculo2');
-const btn = document.querySelector('#btnCalcular');
-const pResult = document.querySelector('#result');
+const h1 = document.querySelector("h1");
+const form = document.querySelector("#form");
+const numero1 = document.querySelector("#calculo1");
+const numero2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+const pResult = document.querySelector("#result");
+const errors = document.querySelector("#errors");
 
+form.addEventListener("submit", sumasion);
 
-
-form.addEventListener('submit', sumasion);
-
-function sumasion(event){
-    event.preventDefault();
-    const sumaTotal = numero1.value + numero2.value;
-    pResult.innerHTML = "Resultado : " + sumaTotal;
+function removeErrors() {
+  errors.classList.remove("showErrors");
+  errors.innerHTML = "";
+  errors.classList.add("hideErrors");
+}
+function addErrors() {
+  pResult.innerHTML = "";
+  errors.innerHTML =
+    "El valor del primer número debe de ser mayor al del segundo";
+  errors.classList.add("showErrors");
 }
 
+function sumasion(event) {
+  event.preventDefault();
+  removeErrors();
+  const { num1, num2 } = {
+    num1: Number(numero1.value),
+    num2: Number(numero2.value)
+  };
 
+  if (num1 <= num2) {
+    addErrors();
+    return;
+  }
 
-
-
-
-
-
-
-
-/*
-btn.addEventListener('click', calculo);
-
-
-function calculo(){
-    const sumaInputs = numero1.value + numero2.value;
-    pResult.append("La suma es " + sumaInputs);
-    }
-
-
-
-console.log(input.value);
-
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input,
-});
-
-
-
-//const img = document.createElement('img');
-//img.setAttribute('src', 'https://static.platzi.com/static/images/conf/logo_black@2x.png');
-console.log(img);
-
-pid.innerHTML("");
-pid.appendChild(img);
-*/
+  const sumaTotal = num1 + num2;
+  pResult.innerHTML = "Resultado : " + sumaTotal;
+}
